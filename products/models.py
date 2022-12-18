cfde441d35c1945abf60a641882c6019e1a9def9
kagwe <kagwepeter07@gmail.com>
2022-12-18 17:02:41 +0300

    sur

---
+++ products/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    added = models.DateTimeField(auto_now_add=True)
+    sold = models.BooleanField(default=False)
